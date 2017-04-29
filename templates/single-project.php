3c1f0b85193d5948ec23d65f30829ec10ef806fb
Rob Stinson <rob@Robs-MacBook-Pro-2.local>
2017-04-29 17:15:14 -0400

    Add .container class to single-project template

--- templates/single-project.php
+++ templates/single-project.php
@@ -17,18 +17,18 @@ get_header(); ?>
 			?>
 			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 				<header class="entry-header">
-					<div class="feature-color" style="background-color: <?php the_field( 'feature_color' ); ?>"></div>
+					<div class="feature-color"></div>
 					<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 					<?php the_field( 'tweet_pitch' ); ?>
 					<?php $flint_plugin->projects->display_field( 'roles' ); ?>
 				</header>
 
-				<div class="entry-content">
+				<div class="entry-content container">
 					<h2><?php _e( 'Overview', 'flint' ); ?></h2>
 					<?php the_content(); ?>
 				</div>
 
-				<div class="entry-content role-descriptions">
+				<div class="entry-content role-descriptions container">
 					<h2><?php _e( 'Team Roles', 'flint' ); ?></h2>
 					<?php
 					while( have_rows( 'roles' ) ) {
@@ -48,7 +48,7 @@ get_header(); ?>
 					<?php $flint_plugin->projects->display_field( 'business_model' ); ?>
 				</div>
 
-				<div class="entry-content timeline">
+				<div class="entry-content timeline container">
 					<?php $flint_plugin->projects->display_field( 'timeline' ); ?>
 				</div>
 
