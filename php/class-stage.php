206c5caeaf3288aa09af9e1d2fece3f61548a554
Luke Carbis <luke@xwp.co>
2017-05-17 12:22:40 +1000

    Add Pending Approval filter to Edit Projects screen

--- php/class-stage.php
+++ php/class-stage.php
@@ -33,6 +33,9 @@ class Stage implements Field_Group {
 			'active' => __( 'Active', 'flint' ),
 			'done' => __( 'Launched', 'flint' ),
 		);
+
+		$plugin = get_plugin_instance();
+		add_filter( 'views_edit-' . $plugin->projects->key, array( $this, 'add_post_view' ) );
 	}
 
 	/**
@@ -66,5 +69,77 @@ class Stage implements Field_Group {
 	public function display_meta_box() {
 		$stage = get_field( $this->key );
 		echo wp_kses_post( $this->stages[ $stage ] );
+	}
+
+	/**
+	 * Add Stage filters to Edit Projects screen.
+	 *
+	 * @filter views_edit-project
+	 *
+	 * @param array $views
+	 * @return array
+	 */
+	public function add_post_view( $views ) {
+		$plugin = get_plugin_instance();
+
+		wp_reset_query();
+		/**
+		 * @see https://core.trac.wordpress.org/ticket/29178
+		 */
+		$args = array(
+			'posts_per_page' => -1,
+			'post_type'      => $plugin->projects->key,
+			'meta_key'       => 'stage',
+			'meta_value'     => 'ready',
+			'fields'         => 'ids',
+			'no_found_rows'  => true,
+		);
+
+		$ready_projects       = new \WP_Query( $args );
+		$ready_projects_count = count( $ready_projects->posts );
+		$ready_projects_class = '';
+		$ready_projects_url   = add_query_arg(
+			array( 'post_type' => $plugin->projects->key, 'stage' => 'ready' ),
+			admin_url( 'edit.php' )
+		);
+
+		$stage = sanitize_key( filter_input( INPUT_GET, 'stage' ) );
+		if ( 'ready' === $stage ) {
+			$ready_projects_class = 'class="current"';
+		}
+
+		$views['ready'] = sprintf(
+			'<a %s href="%s">%s</a> (%d)',
+			$ready_projects_class,
+			$ready_projects_url,
+			__( 'Pending Approval', 'flint' ),
+			$ready_projects_count
+		);
+
+		return $views;
+	}
+
+	/**
+	 * Filter Projects on Edit Projects screen if a Stage view is active
+	 *
+	 * @filter pre_get_posts
+	 *
+	 * @param \WP_Query $query
+	 * @return \WP_Query
+	 */
+	public function filter_post_view( $query ) {
+		global $pagenow;
+
+		if( 'edit.php' !== $pagenow || ! $query->is_admin || 'stage' === $query->get( 'meta_key' ) ) {
+			return $query;
+		}
+
+		$stage = sanitize_key( filter_input( INPUT_GET, 'stage' ) );
+		if ( '' !== $stage ) {
+			$query->set( 'meta_key', 'stage' );
+			$query->set( 'meta_value', $stage );
+		}
+
+		return $query;
 	}
 }
