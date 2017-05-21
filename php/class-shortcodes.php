d0fe42cb4a592f165e293d37d6bca88208b9b4de
Luke Carbis <luke@xwp.co>
2017-05-21 15:47:50 +1000

    Rename Shortcode to Shortcodes

--- php/class-shortcodes.php
+++ php/class-shortcodes.php
@@ -1,28 +1,28 @@
 <?php
 /**
- * Adds shortcode for displaying Flint Projects.
+ * Adds shortcodes for displaying Flint Projects and Updates.
  *
  * @package Flint
  */
 
 namespace Flint;
 
-class Shortcode {
+class Shortcodes {
 
 	/**
-	 * Shortcode constructor.
+	 * Shortcodes constructor.
 	 */
 	public function __construct() {
-		add_shortcode( 'projects', array( $this, 'display' ) );
+		add_shortcode( 'projects', array( $this, 'display_projects' ) );
 	}
 
 	/**
-	 * Print HTML template
+	 * Print HTML template for Projects
 	 *
 	 * @param array $atts
 	 * @return string
 	 */
-	public function display( $atts ) {
+	public function display_projects( $atts ) {
 		$plugin = get_plugin_instance();
 
 		$atts = shortcode_atts(
@@ -41,7 +41,7 @@ class Shortcode {
 		}
 
 		if ( is_user_logged_in() ) {
-			$meta_key = get_plugin_instance()->projects->likes->meta_key;
+			$meta_key = $plugin->projects->likes->meta_key;
 			$likes = get_user_meta( get_current_user_id(), $meta_key, true );
 
 			if ( 'true' === $atts['likes'] || true === $atts['likes'] ) {
@@ -59,23 +59,23 @@ class Shortcode {
 		if ( $query->have_posts() ) {
 			?>
 			<section class="projects-archive">
-			<?php
-			while ( $query->have_posts() ) {
-				$query->the_post();
-				?>
-				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-					<a href="<?php the_permalink(); ?>"><?php the_title( '<h1 class="entry-title" style="color: ' . get_field( 'feature_color' ) . '">', '</h1>' ); ?></a>
-					<?php $plugin->projects->likes->display(); ?>
-					<div class="description">
-						<?php the_field( 'tweet_pitch' ); ?>
-						<p><a class="learn-more-link" href="<?php the_permalink(); ?>">Learn More</a></p>
-					</div>
-					<?php $plugin->projects->display_field( 'roles' ); ?>
-				</article>
 				<?php
-			}
-			wp_reset_postdata();
-			?>
+				while ( $query->have_posts() ) {
+					$query->the_post();
+					?>
+					<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+						<a href="<?php the_permalink(); ?>"><?php the_title( '<h1 class="entry-title" style="color: ' . get_field( 'feature_color' ) . '">', '</h1>' ); ?></a>
+						<?php $plugin->projects->likes->display(); ?>
+						<div class="description">
+							<?php the_field( 'tweet_pitch' ); ?>
+							<p><a class="learn-more-link" href="<?php the_permalink(); ?>">Learn More</a></p>
+						</div>
+						<?php $plugin->projects->display_field( 'roles' ); ?>
+					</article>
+					<?php
+				}
+				wp_reset_postdata();
+				?>
 			</section>
 			<?php
 		}
