f5de0b7618b7e755d293afd8d0c22699141b20cc
Luke Carbis <luke@xwp.co>
2017-05-21 15:28:24 +1000

    Add better hash support to tabs

--- js/project.js
+++ js/project.js
@@ -1,12 +1,19 @@
 ( function( $ ) {
-	$( 'nav.tabs  a' ).click( function( e ) {
+	$( 'nav.tabs a' ).click( function( e ) {
 		e.preventDefault();
 
 		$( this ).parent().addClass( 'current' );
 		$( this ).parent().siblings().removeClass( 'current' );
 
 		var tab = $( this ).attr( 'href' );
+		window.location.hash = tab;
+
 		$( '.tab-content' ).not( tab ).removeClass( 'current' );
 		$( tab ).addClass( 'current' );
 	});
+
+	var hash = window.location.hash;
+	if( hash ) {
+		$( 'nav.tabs a[href="' + hash + '"]' ).trigger( 'click' );
+	}
 }) ( jQuery );
